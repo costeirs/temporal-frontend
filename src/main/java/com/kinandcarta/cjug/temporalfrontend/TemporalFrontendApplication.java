6e2f166ff8488689d03bfc51684f4e5418384ced
Stephen Costeira <costeirs@users.noreply.github.com>
2023-11-14 13:09:48 -0600

    init

---
+++ src/main/java/com/kinandcarta/cjug/temporalfrontend/TemporalFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.kinandcarta.cjug.temporalfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalFrontendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TemporalFrontendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/kinandcarta/cjug/temporalfrontend/TemporalFrontendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kinandcarta.cjug.temporalfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalFrontendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
